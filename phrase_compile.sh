a8f23ce137e9167e9151122213483429631a6c66
kishori <kishori@kishori-desktop>
2019-02-15 11:41:41 +0530

    updates

--- phrase_compile.sh
+++ phrase_compile.sh
@@ -1,4 +1,5 @@
 cd $HOME_anu_test/
+git pull
 echo "removing out files"
 shell_scripts/remove_out-files.sh
 echo "Compiling anu files"
@@ -8,7 +9,7 @@ cd $HOME_anu_test/miscellaneous/SMT/MINION/alignment
 sh minion_compile.sh
 cd $HOME_anu_test/miscellaneous/SMT/phrasal_alignment
 sh compile.sh
-echo "Compiling rule based parser files"
-cd $rule_based_parser
-sh compile.sh
+#echo "Compiling rule based parser files"
+#cd $rule_based_parser
+#sh compile.sh
 
