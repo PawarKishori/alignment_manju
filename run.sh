fe0c8fed3d941e3612100b5e8673942ea005db20
kishori <kishori@kishori-desktop>
2019-04-16 05:01:01 +0530

    Updated to Manju mam's original state

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 cp -r compile_prog.sh  $1 $2  $HOME_anu_test/miscellaneous/SMT/phrasal_alignment/
-#cp  HINDI_POS_CHUNKER.tgz  $HOME_anu_test/miscellaneous/
-#cd $HOME_anu_test/miscellaneous/
-#tar -xvzf HINDI_POS_CHUNKER.tgz
-#cd $HOME_anu_test/miscellaneous/HINDI_POS_CHUNKER
-#sh compile.sh
+cp  HINDI_POS_CHUNKER.tgz  $HOME_anu_test/miscellaneous/
+cd $HOME_anu_test/miscellaneous/
+tar -xvzf HINDI_POS_CHUNKER.tgz
+cd $HOME_anu_test/miscellaneous/HINDI_POS_CHUNKER
+sh compile.sh
 cd  $HOME_anu_test/miscellaneous/SMT/phrasal_alignment/
 sh  compile_prog.sh
 if [ "$3" != "" ] ; then
