bb05505f95ebd022effece3c71053c29dc637de7
PawarKishori <kishori.pawar@research.iiit.ac.in>
2019-10-24 23:59:33 +0530

    changes

--- run_alignment.sh
+++ run_alignment.sh
@@ -1,4 +1,9 @@
+
 #Anusaaraka_stanford.sh $1 0 True $3 onesent > $1_out 2>&1    ; if domain dic exists in anusaaraka
-time Anusaaraka_stanford.sh $1 0 True general onesent > $1_out 2>&1
+echo "Time taken to run only Anusaaraka:"
+#time Anusaaraka_stanford.sh $1 0 True general onesent > $1_out 2>&1
+#time Anusaaraka_stanford.sh $1 0 True geography onesent > $1_out 2>&1
+time Anusaaraka_stanford.sh $1 0 True computer_science onesent > $1_out 2>&1
+echo "Time taken to run only Anusaaraka_alignment.sh:"
 #Anusaaraka_alignment.sh $1 $2 $3 $4 >> $1_out 
 time Anusaaraka_alignment.sh $1 $2 $3 $4 >> $1_out 2>&1
